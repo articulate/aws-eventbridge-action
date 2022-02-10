f94f91a7ca635e8e048135d16c3187ce175d42a5
Matt Loberg <mloberg@articulate.com>
2022-02-10 08:46:22 -0600

    feat: push an event to AWS EventBridge

---
+++ __tests__/event.test.ts
@@ -0,0 +1,27 @@
+import { getEntry } from '../src/event';
+
+test('returns entry', () => {
+    const output = {
+        $metadata: {},
+        Entries: [
+            {
+                EventId: '1234-abcd-5678',
+                ErrorCode: 'Test Error',
+                ErrorMessage: 'Test error details.',
+            },
+        ],
+    };
+    expect(getEntry(output)).toEqual({
+        EventId: '1234-abcd-5678',
+        ErrorCode: 'Test Error',
+        ErrorMessage: 'Test error details.',
+    });
+});
+
+test('returns entry from empty result', () => {
+    expect(getEntry({ $metadata: {} })).toEqual({
+        EventId: '',
+        ErrorCode: 'Unknown Error',
+        ErrorMessage: 'An unknown error occured.',
+    });
+});
