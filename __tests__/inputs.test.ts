39665a06d81e5da2c968fe2f84cc073d06f5832d
Dan Holmes <dholmes@articulate.com>
2023-04-20 11:16:43 -0500

    chore(local-build): standardizes build and test scripts with other projects
    This includes updates to the Readme, use of prettier, updates to eslint
    config, etc. Also removes lint-staged and directs devs to use npm run
    all.
    
    This is being done as a pre-step to merging in the final dependabot
    change.

--- __tests__/inputs.test.ts
+++ __tests__/inputs.test.ts
@@ -1,34 +1,36 @@
 import { getEvent } from '../src/inputs';
 
 test.each([
-    ['', undefined],
-    ['foo', ['foo']],
-    ['foo,bar', ['foo', 'bar']],
-])("returns an event entry", (resourcesInput, resourcesExpect) => {
-    process.env['INPUT_DETAIL'] = '{"foo":"bar"}';
-    process.env['INPUT_EVENT-BUS-NAME'] = 'test-bus';
-    process.env['INPUT_SOURCE'] = 'com.github.actions';
-    process.env['INPUT_RESOURCES'] = resourcesInput;
-    process.env['INPUT_DETAIL-TYPE'] = 'action-test';
+  ['', undefined],
+  ['foo', ['foo']],
+  ['foo,bar', ['foo', 'bar']],
+])('returns an event entry', (resourcesInput, resourcesExpect) => {
+  process.env['INPUT_DETAIL'] = '{"foo":"bar"}';
+  process.env['INPUT_EVENT-BUS-NAME'] = 'test-bus';
+  process.env['INPUT_SOURCE'] = 'com.github.actions';
+  process.env['INPUT_RESOURCES'] = resourcesInput;
+  process.env['INPUT_DETAIL-TYPE'] = 'action-test';
 
-    expect(getEvent()).toEqual({
-        EventBusName: 'test-bus',
-        Source: 'com.github.actions',
-        Resources: resourcesExpect,
-        DetailType: 'action-test',
-        Detail: '{"foo":"bar"}',
-    });
+  expect(getEvent()).toEqual({
+    EventBusName: 'test-bus',
+    Source: 'com.github.actions',
+    Resources: resourcesExpect,
+    DetailType: 'action-test',
+    Detail: '{"foo":"bar"}',
+  });
 });
 
 test('throws an error if detail is not valid JSON', () => {
-    process.env['INPUT_DETAIL'] = 'foobar';
+  process.env['INPUT_DETAIL'] = 'foobar';
 
-    expect(() => getEvent()).toThrow('detail is not valid JSON');
+  expect(() => getEvent()).toThrow('detail is not valid JSON');
 });
 
 test('throws error if required fields are missing', () => {
-    process.env['INPUT_DETAIL'] = '{}';
-    process.env['INPUT_EVENT-BUS-NAME'] = '';
+  process.env['INPUT_DETAIL'] = '{}';
+  process.env['INPUT_EVENT-BUS-NAME'] = '';
 
-    expect(() => getEvent()).toThrow('Input required and not supplied: event-bus-name');
+  expect(() => getEvent()).toThrow(
+    'Input required and not supplied: event-bus-name',
+  );
 });
